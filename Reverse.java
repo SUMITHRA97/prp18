d8f3290257a6bd99d1b96c055061145553797336
SUMITHRA97 <vsumithra1997@gmail.com>
2017-06-06 10:24:56 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class Reverse
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        StringBuffer sb=new StringBuffer("sumi");
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
