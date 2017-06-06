54717c2638fdd8a844e2e8e4b11bb46c0121b36a
SUMITHRA97 <vsumithra1997@gmail.com>
2017-06-06 11:04:24 +0530

    Create Rev.java

---
+++ Rev.java
@@ -0,0 +1,13 @@
+import java.io.*;
+	import java.util.*;
+	class Rev
+	{
+	    public static void main(String args[])
+	    {
+	        Scanner s=new Scanner(System.in);
+	        StringBuffer sb=new StringBuffer("sumi");
+	        sb.reverse();
+	        System.out.println(sb);
+	    }
+	}
+
