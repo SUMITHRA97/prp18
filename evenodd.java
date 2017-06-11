e2d9828ee25ccd3e50c6430609a4a667acd8c1fe
SUMITHRA97 <vsumithra1997@gmail.com>
2017-06-11 09:22:26 -0700

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class evenodd
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+            int a=sc.nextInt();
+            if(a>0)
+            {
+                System.out.println("even");
+            }
+            if(a<0)
+            {
+                System.out.println("odd");
+            }
+    }
+}
