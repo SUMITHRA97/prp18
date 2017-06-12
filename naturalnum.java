80a2086d010c463a3cd739daaa45cafd1bcc658b
SUMITHRA97 <vsumithra1997@gmail.com>
2017-06-11 23:25:49 -0700

    Create naturalnum.java

---
+++ naturalnum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class naturalnum
+{
+    public static void main(String args[])
+    {
+        int i=1,sum=0;
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        while(i<=n)
+        {
+            sum=sum+i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
