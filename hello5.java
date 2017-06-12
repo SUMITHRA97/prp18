c4f730d3ee4052834b0f23449380d7b3ddc3fe1a
SUMITHRA97 <vsumithra1997@gmail.com>
2017-06-11 21:13:12 -0700

    Create hello5.java

---
+++ hello5.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class hello5
+{
+    public static void main(String args[])
+    {
+    int s=5;
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        for(int i=0;i<s;i++)
+        {
+        System.out.println(s1);
+        }
+        }
+    }
+
